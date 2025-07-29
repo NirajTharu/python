35aa6f6999e5a68f644d9809c117f4d6f7377b44
NirajTharu <nirajc279@gmail.com>
2025-07-29 11:00:44 +0545

    about the list and how to access the list ltems

---
+++ 4_a.List.py
@@ -0,0 +1,14 @@
+# About the list variable and various funcition related to it.
+
+# List is a varible which can hold multiple values of different data types and are created using
+# the square brackets[] and items are seprated usint the comma ','
+
+a=["apple", 22, 23.9,"banana", 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Accessing list items: starts form '0' index  and has negative indexing too,
+# where -1 refers to the last item and -2 refers to the second last item and so on.
+
+print(a)
+print(a[1])
+print(a[0])
+print(a[-1])
