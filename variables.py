96d2fd76e1b1a52102a91d589b34e67081a6b7d6
NirajTharu <nirajc279@gmail.com>
2025-07-29 10:04:37 +0545

    Adding the comment in the variale file

--- variables.py
+++ variables.py
@@ -1,5 +1,6 @@
 # Variable in the python
-# Python is dinamically typed language
+# Python is dinamically typed language means you should not declare the type of a variable explicitly. 
+# A variable is created at the moment a value is assigned to it.
 
 a = "apple"
 a = 5
