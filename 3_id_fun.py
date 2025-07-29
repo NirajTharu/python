0c476485c0ea0783f7952c0b4aa1115d3e13008b
NirajTharu <nirajc279@gmail.com>
2025-07-29 10:36:59 +0545

    The id() function in the python

---
+++ 3_id_fun.py
@@ -0,0 +1,7 @@
+# Id() function is used to assign unique identifier to Objects not variables is same like pointer in the c, c++.
+
+a = 10
+b = 20
+c = 10
+print("The unique id of a is",id(a))
+print("The unique id of b is",id(b))
